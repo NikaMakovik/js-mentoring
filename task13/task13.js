b3cf5920e8b2853786c6804918e20902c71eab8a
Veranika Makovik <Veranika_Makovik@epam.com>
2020-03-04 17:45:19 +0300

    changes

--- task13/task13.js
+++ task13/task13.js
@@ -1,6 +1,6 @@
-describe("Comparison of", function() {
-    it("two numbers", function() {
-      let a = 6;
+describe("Comparison of", () => {
+    it("two numbers", () => {
+      const a = 6;
       expect(a).toBe(6);
       expect(a).toBeGreaterThan(5);
       expect(a).toBeLessThan(7);
@@ -9,34 +9,34 @@ describe("Comparison of", function() {
       expect(a).not.toBeLessThan(5);
     });
 
-    it("two lines for a complete match", function() {
-      let a = 'abba';
-      let b = 'abba';
-      let c = 'sfgds';
+    it("two lines for a complete match", () => {
+      const a = 'abba';
+      const b = 'abba';
+      const c = 'sfgds';
       expect(a).toBe(b);
       expect(a).toEqual(b);
       expect(a).not.toBe(c);
       expect(c).not.toEqual(b);
     });
 
-    it("two lines for a partial match (contains)", function() {
-      let a = 'habbah';
-      let b = 'abba';
-      let c = 'gggg';
+    it("two lines for a partial match (contains)", () => {
+      const a = 'habbah';
+      const b = 'abba';
+      const c = 'gggg';
       expect(a).toMatch(b);
       expect(a).not.toMatch(c);
     });
 
-    it("arrays", function() {
-      let arr1 = ['Alice', 'Bob', 4, 8];
-      let arr2 = ['Alice', 'Bob', 4, 8];
+    it("arrays", () => {
+      const arr1 = ['Alice', 'Bob', 4, 8];
+      const arr2 = ['Alice', 'Bob', 4, 8];
       expect(arr1).not.toBe(arr2);
       expect(arr1).toEqual(arr2);
     });
 
-    it("array of strings", function() {
-      let arr1 = ['Alice', 'Bob'];
-      let arr2 = ['Alice', 'Bob'];
+    it("array of strings", () => {
+      const arr1 = ['Alice', 'Bob'];
+      const arr2 = ['Alice', 'Bob'];
       expect(arr1).not.toBe(arr2);
       expect(arr1).toContain('Alice');
       expect(arr1).toEqual(arr2);
